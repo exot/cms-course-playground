3206cfc71188614bbdd231b17979e37f72abf545
vv01f <vv01f@users.noreply.github.com>
2018-09-08 17:10:55 +0200

    fix fn

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 which pdflatex || { echo "pdflatex missing."; exit 1; }
 which youtube-dl || { echo "youtube-dl missing."; exit 1; }
 # get dependencies
-fn=img-lion.pdf
-test -f ${fn} || pdflatex ${fn}
-fn=ThisLandIsMine-byNinaPaley.mkv
+fn="img-lion"
+test -f ${fn}".pdf" || pdflatex ${fn}".tex"
+fn="ThisLandIsMine-byNinaPaley"
 test -f ${fn} || youtube-dl -o ${fn} https://www.youtube.com/watch?v=4pKMV6e5kEo
-# build keynote
+# build keynote, regardless if there or not
 fn=beamer-presentation
 pdflatex ${fn}".tex"
 pdflatex ${fn}".tex"
